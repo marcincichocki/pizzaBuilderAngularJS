4fba8083da641cbdea71d0014ad90ba75be18dab
marcincichocki <poczta@marcincichocki.net>
2015-09-13 15:08:51 +0200

    Devided app into directories

---
+++ assets/js/controllers/tab-controller.js
@@ -0,0 +1,35 @@
+
+
+
+angular.module('pizzaBuilder')
+
+	/**
+	 * tab controller(switching)
+	 */
+	.controller('TabController', [function() {
+
+
+		// active tab
+		this.current = 0;
+
+		/**
+		 * Check if given tab is selected
+		 * @param  {Number}  tab ngRepeat $index of <li>
+		 * @return {Boolean}     if tab is selected return true, otherwise false
+		 */
+		this.isSelected = function(tab) {
+			return this.current === tab;
+		};
+
+		/**
+		 * Select tab
+		 * @param  {Number} tab $index of tab
+		 */
+		this.selectTab = function(tab) {
+			this.current = tab;
+		};
+
+
+	}]);
+
+
