68901841ff9acf67130605446f2083b6fec56917
marcincichocki <poczta@marcincichocki.net>
2015-09-13 14:59:48 +0200

    Devided app into directories

--- assets/js/controllers/new-controller.js
+++ assets/js/controllers/new-controller.js
@@ -1,42 +1,43 @@
-(function () {
-	
 
 
-	angular.module('pizzaBuilder')
+
+
+angular.module('pizzaBuilder')
+
+	/**
+	 * main contorller of /new route
+	 */
+	.controller('NewController', ['$scope', '$location', 'Components', 'Data', function($scope, $location, Components, Data) {
+		
+
+		// some data to fill inputs in view
+		$scope.title = 'Dodaj';
 
 		/**
-		 * main contorller of /new route
+		 * Add new pizza to pizzaList
+		 * 
+		 * @param {String} name name of pizza
 		 */
-		.controller('NewController', ['$scope', '$location', 'Components', 'Data', function($scope, $location, Components, Data) {
-			
-
-			// some data to fill inputs in view
-			$scope.title = 'Dodaj';
-
-			/**
-			 * Add new pizza to pizzaList
-			 * 
-			 * @param {String} name name of pizza
-			 */
-			$scope.save = function(name) {
-				var pizza = {
-					name: name,
-					components: Components.getSelection()
-				};
-
-				if ($scope.$parent.useExpress) {
-					Data
-						.post('http://localhost:' + Data.PORT + '/list', pizza)
-						.then(function(response) {
-							$location.path('/');
-							$scope.$parent.pizzaList.push(pizza);
-						});
-				} else {
-					$scope.$parent.pizzaList.push(pizza);
-					$location.path('/');
-				}
+		$scope.save = function(name) {
+			var pizza = {
+				name: name,
+				components: Components.getSelection()
 			};
 
-		}]);
+			if ($scope.$parent.useExpress) {
+				Data
+					.post('http://localhost:' + Data.PORT + '/list', pizza)
+					.then(function(response) {
+						$location.path('/');
+						$scope.$parent.pizzaList.push(pizza);
+					});
+			} else {
+				$scope.$parent.pizzaList.push(pizza);
+				$location.path('/');
+			}
+		};
 
-}());
+	}]);
+
+
+
